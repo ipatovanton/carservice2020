cbae631ce2eb62ebc1b2d2412bae34a8d0bba510
ipatovanton <ipatovanton@gmail.com>
2020-01-23 20:42:03 +0300

    fix

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -5,10 +5,13 @@ class Setting
 
   field :title, type: String, default: 'Подорожник'
   field :description, type: String, default: 'СРОЧНАЯ ТЕХНИЧЕСКАЯ ПОМОЩЬ ЗА 20 МИНУТ'
-  field :phone
+  field :phone, default: '84951111111'
   field :whatsapp
   field :telegram
   field :viber
+
+  field :seotitle, type: String, default: 'Подорожник'
+  field :seodescription, type: String, default: 'СРОЧНАЯ ТЕХНИЧЕСКАЯ ПОМОЩЬ ЗА 20 МИНУТ'
 
   field :script
 
@@ -16,4 +19,8 @@ class Setting
 
   validates :title, presence: true
   validates :description, presence: true
+  validates :image, presence: true
+  validates :phone, presence: true
+  validates :seotitle, presence: true
+  validates :seodescription, presence: true
 end
