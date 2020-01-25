698e99194562ead81816e25776fafe0b4cf8863f
ipatovanton <ipatovanton@gmail.com>
2020-01-25 12:40:49 +0300

    fix

--- config/application.rb
+++ config/application.rb
@@ -34,8 +34,8 @@ module Carservice
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*', 'https://d1xb6unwu6rjq8.cloudfront.net'
-        resource '*', headers: :any, methods: [:get]
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
     
